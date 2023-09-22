42d37b9d1cf2d33d9938bc75132e99001976dcb6
Cole <colehausman@gmail.com>
2023-09-22 10:52:08 -0400

    minor search patch

--- ezgrant/src/backend/services/query-parser.js
+++ ezgrant/src/backend/services/query-parser.js
@@ -3,10 +3,8 @@ const oracledb = require('oracledb');
 function generate_query(){
     const columnsToCheck = ['name', 'location', 'about', 'eligibility'];
 
-    const sqlConditions = userQueryTerms.map(term => {
-        return columnsToCheck.map(column => {
-          return `UPPER(${column}) LIKE '%' || '${term}' || '%'`;
-        }).join(' OR ');
+    const sqlConditions = columnsToCheck.map(column => {
+        return `UPPER(${column}) LIKE :keywords`;
       });
     
     const sqlStatement = `
@@ -22,7 +20,7 @@ module.exports.generate_query = generate_query;
 
 function get_binds(user_query){
     user_query = user_query.replace(/\s+/g,' ').trim().toUpperCase();
-    return {keywords: { dir: oracledb.BIND_IN, val: user_query, type: oracledb.STRING }}
+    return {keywords: { dir: oracledb.BIND_IN, val: `%${user_query}%`, type: oracledb.STRING }}
 }
 
 module.exports.get_binds = get_binds;
