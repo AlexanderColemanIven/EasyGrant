de8b1c22b5125b7188a09f8136dc96ce7990da36
Cole <colehausman@gmail.com>
2023-12-09 16:31:26 -0500

    azure fixes

--- ezgrant/src/backend/server.js
+++ ezgrant/src/backend/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ORACLEDB_DRIVER_MODE === 'thick') {
 }
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
